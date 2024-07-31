e4226e41e513d6005b47e8fedc35e50e0a6fe1af
Sahithi2903 <Nallagundlasahithi@gmail.com>
2024-07-31 12:12:17 +0530

    Update SingletonPatternExample.java

--- Week_1_Exercises/Design_Patterns_and_Principles/SingletonPatternExample.java
+++ Week_1_Exercises/Design_Patterns_and_Principles/SingletonPatternExample.java
@@ -1,1 +1,36 @@
+class Logger {
+    private static Logger instance;
 
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            synchronized (Logger.class) {
+                if (instance == null) {
+                    instance = new Logger();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("Log: " + message);
+    }
+}
+
+public class SingletonPatternExample {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("First log message.");
+        logger2.log("Second log message.");
+
+        if (logger1 == logger2) {
+            System.out.println("Logger1 and Logger2 are the same instance.");
+        } else {
+            System.out.println("Logger1 and Logger2 are different instances.");
+        }
+    }
+}
